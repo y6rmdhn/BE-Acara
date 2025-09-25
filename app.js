9af6c9389ab18b6b48182e733ee4ba75bc67e17b
Muhamad Yopan Ramadhan <yopanramadhan8@gmail.com>
2025-09-25 11:12:35 +0700

    fix

--- app.js
+++
@@ -1,6 +0,0 @@
-import crypto from 'crypto';
-
-const SECRET = crypto.randomBytes(16).toString('hex');
-
-
-console.log(`Secret: ${SECRET}`);
--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,6 @@
 import express from "express";
 import dotenv from "dotenv";
-import router from "./routes/api";
+import router from "./routes/api.js";
 import bodyParser from "body-parser";
 import db from "./utils/database";
 
